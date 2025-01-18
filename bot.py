646cd53cd017acace9ab33a67a0f8b71998c423e
empester <secur2008treb@gmail.com>
2025-01-18 19:23:10 +0200

    desync, in work

--- bot.py
+++ bot.py
@@ -5,13 +5,26 @@ import os
 
 load_dotenv()
 
+GUILD_ID = 1322769222639816707
+
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!",intents=intents)
-bot.remove_command('help')
-
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"), intents=intents)
 TOKEN = os.getenv("TOKEN")
+
+@bot.hybrid_command(name='ping', with_app_command=True)
+async def ping(ctx):
+    latency = round(bot.latency*1000)
+    await ctx.send(f"Ping: {latency}ms")
+
+@bot.command(name='desync', with_app_command=False)
+async def desync(ctx):
+    guild = discord.Object(id=GUILD_ID)
+    bot.tree.clear_commands(guild=guild)
+    await bot.tree.sync(guild=guild)
+    await ctx.send("bot.tree.clear_commands(guild=guild) was a success.")
+
 
 @bot.event
 async def on_ready():
@@ -21,4 +34,11 @@ async def on_ready():
     for intro_of_bot in bot_intro:
         print("Bot...".ljust(11) + intro_of_bot)
 
+
+async def setup_hook():
+    guild = discord.Object(id=GUILD_ID)
+    bot.tree.copy_global_to(guild=guild)
+    await bot.tree.sync(guild=guild)
+
+bot.setup_hook=setup_hook
 bot.run(TOKEN)
